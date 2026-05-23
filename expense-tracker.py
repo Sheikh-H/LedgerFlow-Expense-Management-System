df745fbdb5827442d907cdb49826d3c5cf44a2c6
Sheikh Hussain <sheikh.hussain1155@gmail.com>
2026-05-23 23:58:46 +0100

    trying out the csv module to see what we could use on this. I will have a look at the documentation later and find the proper use for it. Right now, the file is made but nothing is written into the file.

--- expense-tracker.py
+++ expense-tracker.py
@@ -2,3 +2,15 @@ import os
 import sys
 import datetime
 import csv
+
+
+with open("expenses.csv", 'w') as f:
+    csv.DictWriter(f,['field1','field2'])
+    with open("expenses.csv", 'r') as f:
+        data = csv.DictReader(f)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
